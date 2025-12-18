1eafb96893f3a10a35e7a0d0ac9f63808296e533
Igor Lima <igorlimafac@gmail.com>
2025-12-18 17:36:21 -0300

    fix gitignore

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,22 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { initializeFirestore, CACHE_SIZE_UNLIMITED } from "firebase/firestore"; // Mudamos aqui
+import { initializeFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyABh0nqY_YJFBOAsKOAuVwYjm2soRVaEG4",
-  authDomain: "igor-portfolio-cms.firebaseapp.com",
-  projectId: "igor-portfolio-cms",
-  storageBucket: "igor-portfolio-cms.firebasestorage.app",
-  messagingSenderId: "269561809266",
-  appId: "1:269561809266:web:a9a39c5fea55f775aff5eb",
-  measurementId: "G-XK8JR4995Q"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
 
-// Configuração otimizada para evitar delay em redes locais/desenvolvimento
+// Mantendo sua configuração otimizada de Long Polling para estabilidade
 export const db = initializeFirestore(app, {
-  experimentalForceLongPolling: true, // Força um método de conexão mais estável para desenvolvimento
+  experimentalForceLongPolling: true,
 });
 
 export const auth = getAuth(app);
